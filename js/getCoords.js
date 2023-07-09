056309a7fb40a7b5cade1b8912a03bb9348dcd32
unknown <simon castillo>
2023-07-09 15:49:38 +0200

    working in search function v.18

--- js/getCoords.js
+++ js/getCoords.js
@@ -2,10 +2,11 @@
 
 const search_bar = document.getElementById("location");
 const form = document.getElementById("form");
-const geocodeApiUrl = "https://api.opencagedata.com/geocode/version/json?key=43989616dfc84015b6fe9bb9ff53db9e";
+
 
 
 form.addEventListener("submit", (e) => {
+    const geocodeApiUrl = "https://api.opencagedata.com/geocode/version/json?key=43989616dfc84015b6fe9bb9ff53db9e";    
     e.preventDefault();
-
+    fetch()
 })
